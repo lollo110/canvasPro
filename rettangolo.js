e9c55c7aa13db54206faa24916b6f35ecd7adbf0
lollo110 <lorenzopaolocilurzo@gmail.com>
2026-01-08 16:47:58 +0100

    rotazione + fori(da rifare)

--- rettangolo.js
+++ rettangolo.js
@@ -6,38 +6,100 @@ canva.height = window.innerHeight -20;
 
 
 class Rettangolo {
-  constructor(x, y, dy, width, height, color, rotationj) {
+  constructor(x, y, dy, width, height, rotation, rotationSpeed, maxRotation, translationX, maxTranslationX) {
     this.x = x;
     this.y = y;
 
     this.dy = dy;
     this.width = width;
     this.height = height;
-    this.color = color;
-    this.rotation = this.rotation;
+    this.rotation = rotation;
+    this.rotationSpeed = rotationSpeed;
+    this.maxRotation = maxRotation;
+    this.translationX = translationX;
+    this.maxTranslationX = maxTranslationX;
+    this.cellSize = 10;
+    this.thread = 6;
+    this.hole = 6;
 
   }
 
   draw() {
-    context.beginPath();
-    context.fillStyle = this.color;
-    context.stroke();
-    context.strokeStyle = "black";
 
-    context.fillRect(this.x, this.y, this.width, this.height);
-    context.strokeRect(this.x, this.y, this.width, this.height);
-    context.closePath();
+    context.save();
+    
+    const cx = this.x + this.width / 2;
+    const cy = this.y + this.height / 2;
+    context.translate(cx,cy);
+    context.rotate(this.rotation);
+    
+
+    context.fillStyle = "#c5babaff";
+
+    context.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+    context.strokeRect(-this.width / 2, -this.height / 2, this.width, this.height);
+
+    const startX = -this.width / 2;
+    const startY = -this.height / 2;
+
+    context.fillStyle = "#ffffff";
+    for (let x = startX; x < this.width / 2; x += this.cellSize) {
+      context.fillRect(
+        x + (this.cellSize - this.thread) / 2,
+        startY,
+        this.thread,
+        this.height
+      );
+    }
+
+    for (let y = startY; y < this.height / 2; y += this.cellSize){
+      context.fillRect(
+        startX,
+        y + (this.cellSize - this.thread) / 2,
+        this.width,
+        this.thread
+      );
+      
+    }
+
+    context.clearRect(0, 0, 0, 0);
+    for (let y = startY; y < this.height / 2; y += this.cellSize){
+      for (let x = startX; x < this.width / 2; x += this.cellSize) {
+        context.clearRect(
+          x + (this.cellSize - this.hole) / 2,
+          y + (this.cellSize - this.hole) / 2,
+          this.hole,
+          this.hole
+        );
+        
+      }
+    }
+    context.strokeStyle='black';
+    context.strokeRect(startX, startY, this.width, this.height);
+    
+    
+    
+    context.restore();
   }
   update() {
     let targetY = canva.height / 2;
 
-    if (this.y + this.height >= targetY){
+    if(!this.isStopped){
+       if (this.y + this.height >= targetY){
         this.y = targetY -this.height;
-     } 
+        this.isStopped = true;
+     } else{
+       this.y += this.dy;
+     }
 
-
-    this.y += this.dy;
-
+    } else {
+      if(Math.abs(this.rotation) < Math.abs(this.maxRotation)){
+        this.x += this.translationX;
+        this.rotation += this.rotationSpeed;
+      }
+      
+    }
+   
 
     this.draw();
   }
@@ -46,12 +108,12 @@ class Rettangolo {
 let rett;
 let rett2;
 function init() {
-  let x = canva.width / 2 - 250;
+  let x = canva.width / 2 - 500;
   let x2 = canva.width / 2 ;
   var dy = 5;
   let y = 0;
-  rett = new Rettangolo(x, y, dy, 500, 100, "green");
-  rett2 = new Rettangolo(x2, y, dy, 500, 100, "red");
+  rett = new Rettangolo(x, y, dy, 500, 100, 0,0.02, Math.PI / 12,20);
+  rett2 = new Rettangolo(x2, y, dy, 500, 100, 0,-0.02,-Math.PI / 12,-20);
 }
 
 function animate(){
